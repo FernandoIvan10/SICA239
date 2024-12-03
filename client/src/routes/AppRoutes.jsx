6769648dd0ad971751248adf2d8465586734102c
FernandoIvan <fascenciocortes@gmail.com>
2024-12-03 08:01:39 -0600

    Se implementó la configuración básica de las rutas (URL)

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import Inicio from "../pages/Inicio"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+
+// Clase que maneja las rutas (URL) de la App
+export default function AppRoutes(){
+    return(
+        // Ruta con su página asociada
+        <Router>
+            <Routes>
+                {/* Ruta por defecto */}
+                <Route path="*" element={<Navigate to="/Inicio" />} />
+                <Route path="/Inicio" element={<Inicio/>}/>
+            </Routes>
+        </Router>
+    )
+}

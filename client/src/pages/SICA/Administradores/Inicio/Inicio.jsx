8bdb57f3959d0831c22c378eda1f117a7c1528dc
FernandoIvan <fascenciocortes@gmail.com>
2025-01-10 15:12:42 -0600

    Se agregó la validación del token al panel de administradores en el apartado Inicio

--- client/src/pages/SICA/Administradores/Inicio/Inicio.jsx
+++ client/src/pages/SICA/Administradores/Inicio/Inicio.jsx
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode"
 import MenuLateral from "../../../../components/sica/MenuLateral/MenuLateral";
 import { FaHouseChimney } from "react-icons/fa6";
 import { FaFileUpload, FaUsers, FaUserEdit, FaLayerGroup } from "react-icons/fa";
@@ -5,9 +6,39 @@ import { TiUserAdd } from "react-icons/ti";
 import { IoLogOut } from "react-icons/io5";
 import Bienvenida from "../../../../components/sica/Bienvenida/Bienvenida";
 import "./Inicio.css"
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 // Página de inicio del SICA para administradores
 export default function InicioAdmin(){
+    const navigate = useNavigate() // Para redireccionar a los usuarios
+    const [nombre, setNombre] = useState('') // Nombre del usuario
+
+    useEffect(() => {
+        const token = localStorage.getItem('token') // Token de inicio de sesión
+
+        // Se valida que el usuario haya iniciado sesión
+        if(!token){
+            // Si el usuario no ha iniciado sesión es redirigido al login
+            navigate('/SICA/iniciar-sesion')
+        } else {
+            try{
+                const tokenDecodificado = jwtDecode(token) // Se decodifica el token
+                setNombre(tokenDecodificado.nombre); // Se almacena el nombre en el estado
+
+                if(tokenDecodificado.rol !== 'superadmin'
+                    && tokenDecodificado.rol !== 'editor'
+                    && tokenDecodificado.rol !== 'lector'){
+                        // Si el usuario es un alumno se redirige a su panel
+                        navigate('/SICA/alumnos/inicio')
+                }
+            }catch(error){
+                // Si hay algún error se redirige al usuario al inicio de sesión
+                navigate('/SICA/iniciar-sesion')
+            }
+        }
+    }, [navigate])
+
     const elementos=[ // Elementos del menú
         {titulo: "Inicio", icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
         {titulo: "Subir calificaciones", icono:FaFileUpload, link:'/SICA/administradores/subir-calificaciones'},
@@ -24,7 +55,7 @@ export default function InicioAdmin(){
         <div className="contenedor-inicio">
             <MenuLateral elementos={elementos}/>
             <Bienvenida 
-                nombre="[NOMBRE]" 
+                nombre={nombre} 
                 descripcion="Este es el sistema de calificaciones, aquí podrás gestionar los usuarios,
                 gestionar los grupos y subir calificaciones."
                 boton1="Subir calificaciones"

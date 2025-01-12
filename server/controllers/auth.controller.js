3c74a5ec8772db24cf51005ea9318022b613cb3c
FernandoIvan <fascenciocortes@gmail.com>
2025-01-11 20:45:01 -0600

    Se agregaron el nombre y apellido al token

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -38,7 +38,7 @@ const loginController = async(req,res)=>{
 
         // Se genera un token de sesión
         const token = jwt.sign(
-            {id:user._id, rol},
+            {id:user._id, rol, nombre: user.nombre, apellido: user.apellido},
             process.env.CLAVE_SECRETA,
             {expiresIn:'3h'} // Tiempo de expiración
         )

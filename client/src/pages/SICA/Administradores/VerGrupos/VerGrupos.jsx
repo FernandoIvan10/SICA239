689c631ec153b131383cd18b9c967008faed4a3c
FernandoIvan <fascenciocortes@gmail.com>
2025-05-26 12:45:52 -0600

    Se agregó la funcionalidad para mostrar los grupos en la página VerGrupos

--- client/src/pages/SICA/Administradores/VerGrupos/VerGrupos.jsx
+++ client/src/pages/SICA/Administradores/VerGrupos/VerGrupos.jsx
@@ -1,38 +1,117 @@
+import {jwtDecode} from "jwt-decode"
 import MenuLateral from "../../../../components/sica/MenuLateral/MenuLateral";
+import "./VerGrupos.css"
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useValidarToken } from "../../../../hooks/useValidarToken/useValidarToken";
+
 import { FaHouseChimney } from "react-icons/fa6";
 import { FaFileUpload, FaUsers, FaUserEdit, FaLayerGroup } from "react-icons/fa";
 import { TiUserAdd } from "react-icons/ti";
 import { IoLogOut } from "react-icons/io5";
+import { MdGroupAdd, MdGroups } from "react-icons/md";
+import { RiCalendarScheduleFill } from "react-icons/ri";
 import { MdEdit, MdDelete } from "react-icons/md";
-import "./VerGrupos.css"
-import { useState } from "react";
 
-// Página del sica para ver la lista de usuarios
+// Página del sica para ver la lista de grupos
 export default function VerGrupos(){
-    const elementos=[ // Elementos del menú
-        {titulo: "Inicio", icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
-        {titulo: "Subir calificaciones", icono:FaFileUpload, link:'/SICA/administradores/subir-calificaciones'},
-        {titulo: "Gestionar usuarios", icono:FaUsers, 
-            subelementos:[
-                {titulo:"Agregar usuario", icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
-                {titulo:"Ver usuarios", icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
-            ]},
-        {titulo: "Gestionar grupos", icono:FaLayerGroup, link:'/SICA/administradores/gestionar-grupos'},
-        {titulo: "Cerrar sesión", icono:IoLogOut, link:'/inicio'},
-    ]
+    const navigate = useNavigate() // Para redireccionar a los usuarios
+    const [menu, setMenu] = useState([]) // Elementos del menú
+    const [grupos, setGrupos] = useState([]); // Grupos del sistema
 
-    // Simulación de grupos
-    const [grupos, setGrupos] = useState([
-        { id: 1, nombre: "Grupo 1", materias: ["Matemáticas", "Ciencias"] },
-        { id: 2, nombre: "Grupo 2", materias: ["Historia", "Geografía"] },
-    ]);
+    useValidarToken() // Se valida que el usuario haya iniciado sesión
+
+    useEffect(() => {
+        const token = localStorage.getItem('token') // Token de inicio de sesión        
+            try{
+                const tokenDecodificado = jwtDecode(token) // Se decodifica el token
+
+                if(tokenDecodificado.rol === 'alumno'){
+                        // Si el usuario es un alumno se redirige a su panel
+                        navigate('/SICA/alumnos/inicio')
+                }
+
+                if(tokenDecodificado.rol === 'superadmin'){
+                    // Si el usuario es superadmin
+                    // Se asigna el siguiente menú
+                    setMenu([ 
+                        {titulo: "Inicio", icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
+                        {titulo: "Subir calificaciones", icono:FaFileUpload, link:'/SICA/administradores/subir-calificaciones'},
+                        {titulo: "Gestionar usuarios", icono:FaUsers, 
+                            subelementos:[
+                                {titulo:"Agregar usuario", icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
+                                {titulo:"Ver usuarios", icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
+                            ]},
+                        {titulo: "Gestionar grupos", icono:FaLayerGroup, 
+                            subelementos:[
+                                {titulo:"Agregar grupo", icono:MdGroupAdd, link:'/SICA/administradores/agregar-grupo'},
+                                {titulo:"Ver grupos", icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
+                            ]},
+                        {titulo: "Subir horarios", icono:RiCalendarScheduleFill, link:'/SICA/administradores/subir-horarios'},
+                        {titulo: "Cerrar sesión", icono:IoLogOut, link:'/inicio'},
+                    ])
+                }else if(tokenDecodificado.rol==='editor'){
+                    // Si el usuario es editor
+                    // Se asigna el siguiente menú
+                    setMenu([ 
+                        {titulo: "Inicio", icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
+                        {titulo: "Subir calificaciones", icono:FaFileUpload, link:'/SICA/administradores/subir-calificaciones'},
+                        {titulo: "Gestionar usuarios", icono:FaUsers, 
+                            subelementos:[
+                                {titulo:"Agregar usuario", icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
+                                {titulo:"Ver usuarios", icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
+                            ]},
+                        {titulo: "Gestionar grupos", icono:FaLayerGroup, 
+                            subelementos:[
+                                {titulo:"Agregar grupo", icono:MdGroupAdd, link:'/SICA/administradores/agregar-grupo'},
+                                {titulo:"Ver grupos", icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
+                            ]},
+                        {titulo: "Cerrar sesión", icono:IoLogOut, link:'/inicio'},
+                    ])
+                } else if(tokenDecodificado.rol==='lector'){
+                    // Si el usuario es lector se asigna el siguiente menú
+                    setMenu([ 
+                        {titulo: "Inicio", icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
+                        {titulo: "Calificaciones", icono:FaFileUpload, link:'/SICA/administradores/subir-calificaciones'},
+                        {titulo:"Usuarios", icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
+                        {titulo: "Grupos", icono:FaLayerGroup, link:'/SICA/administradores/gestionar-grupos'},
+                        {titulo: "Cerrar sesión", icono:IoLogOut, link:'/inicio'},
+                    ])
+                }
+                obtenerGrupos();
+            }catch(error){
+                // Si hay algún error se redirige al usuario al inicio de sesión
+                navigate('/SICA/iniciar-sesion')
+            }
+    }, [navigate])
+
+    // Método para obtener los grupos del backend
+    const obtenerGrupos = () => {
+        const token = localStorage.getItem("token"); //Token de inicio de sesión
+        fetch('http://localhost:3000/api/grupos',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(async res => {
+            if (res.ok) { // Si se obtienen los grupos correctamente
+                const data = await res.json();
+                setGrupos(data.grupos);
+                return
+            }else{ // Si ocurrió un error se alerta al usuario
+                console.error(`Error ${res.status}`, await res.json().catch(()=>null))
+                alert("Ocurrió un error al obtener los grupos")
+                return   
+            }
+        })
+    }
 
     return(
         <div className="contenedor-gestionar-grupos">
-            <MenuLateral elementos={elementos}/>
+            <MenuLateral elementos={menu}/>
             <div className="contenido-principal">
                 <h1>Gestionar Grupos</h1>
-                {/* Tabla de grupos */}
                 <table className="tabla-grupos">
                     <thead>
                         <tr>
@@ -47,7 +126,7 @@ export default function VerGrupos(){
                             <tr key={grupo.id}>
                                 <td>{index + 1}</td>
                                 <td>{grupo.nombre}</td>
-                                <td>{grupo.materias.join(", ")}</td>
+                                <td>{grupo.materias.map((materia) => materia.nombre).join(', ')}</td>
                                 <td>
                                     <MdEdit className="btn-editar"/>
                                     <MdDelete className="btn-eliminar"/>
@@ -56,9 +135,9 @@ export default function VerGrupos(){
                         ))}
                     </tbody>
                 </table>
-                {/* Botón para agregar un nuevo grupo */}
                 <button
                     className="btn-agregar-grupo"
+                    onClick={navigate('/SICA/administradores/agregar-grupo')}
                 >
                     Agregar Grupo
                 </button>

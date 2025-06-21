9cb576fe3d953a449ce6dc54a2109043b4f92807
FernandoIvan <fascenciocortes@gmail.com>
2025-06-21 15:01:40 -0600

    Se corrigió un error en el controlador de Alumnos

--- server/controllers/alumnos.controller.js
+++ server/controllers/alumnos.controller.js
@@ -1,6 +1,7 @@
 // imports
 const Alumno = require('../models/alumno.model')
 const Grupo = require('../models/grupo.model')
+const Materia = require('../models/materia.model')
 const bcrypt = require('bcrypt')
 
 // Función para agregar un nuevo alumno

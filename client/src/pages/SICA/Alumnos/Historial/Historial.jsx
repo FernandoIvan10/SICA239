a56a0ef6b98d8602820aee1851a3c3dd8567f35c
FernandoIvan <fascenciocortes@gmail.com>
2025-07-18 08:27:48 -0600

    Se modificó el CSS de la página Historial

--- client/src/pages/SICA/Alumnos/Historial/Historial.jsx
+++ client/src/pages/SICA/Alumnos/Historial/Historial.jsx
@@ -1,44 +1,44 @@
-import MenuLateral from "../../../../components/sica/MenuLateral/MenuLateral"
-import { useValidarToken } from "../../../../hooks/useValidarToken/useValidarToken"
-import { useValidarRol } from "../../../../hooks/useValidarRol/useValidarRol"
-import './Historial.css'
-import { useEffect } from "react"
-import { jwtDecode } from "jwt-decode"
-import { useState } from "react"
+import MenuLateral from '../../../../components/sica/MenuLateral/MenuLateral'
+import { useValidarToken } from '../../../../hooks/useValidarToken/useValidarToken'
+import { useValidarRol } from '../../../../hooks/useValidarRol/useValidarRol'
+import { useEffect } from 'react'
+import { jwtDecode } from 'jwt-decode'
+import { useState } from 'react'
+import '../../../../assets/styles/global.css'
 
 // Página de inicio del SICA para alumnos
 export default function Historial(){
     useValidarToken() // El usuario debe haber iniciado sesión
     useValidarRol(['alumno']) // El usuario debe tener permiso para acceder a esta ruta
 
-    const token = localStorage.getItem("token") // Token de inicio de sesión
+    const token = localStorage.getItem('token') // Token de inicio de sesión
     const tokenDecodificado = jwtDecode(token) // Datos del token
     const [historial, setHistorial] = useState([]) // Historial académico
 
     useEffect(() => { // Se obtiene el historial académico del alumno
         try{
             fetch(`/api/historial-academico/${tokenDecodificado.id}`, {
-                method: "GET",
+                method: 'GET',
                 headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
                 }
             }).then(async res => {
                 const data = await res.json()
                 if(res.ok){
                     setHistorial(data[0].calificaciones)
                 }else{
-                    alert(data.mensaje || "Error al obtener el historial académico")
+                    alert(data.mensaje || 'Error al obtener el historial académico')
                 }
             })
         }catch{
-            console.error("Error al obtener el historial académico:", error)
-            alert("No se pudo obtener el historial académico.")
+            console.error('Error al obtener el historial académico:', error)
+            alert('No se pudo obtener el historial académico.')
         }
     }, [])
 
     return(
-        <div className="contenedor-inicio">
+        <div className="contenedor-principal">
             <MenuLateral/>
             <div className="contenido-principal">
                 <h1>Historial Academico</h1>

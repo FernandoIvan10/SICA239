90da9e0b1ff1f57e19899e986eb98ed1d3349c03
FernandoIvan <fascenciocortes@gmail.com>
2026-01-07 10:35:04 -0600

    normalización de modelo horario

--- server/models/horario.model.js
+++ server/models/horario.model.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
+// Esquema de la colección de los horarios
 const HorarioSchema = new mongoose.Schema({
   grupo: { type: mongoose.Schema.Types.ObjectId, ref: 'Grupo', required: true },
-  imagenUrl: String,
-  publicId: String
-}, { collection: 'horarios' })
+  imagenUrl: { type: String, required: true },
+  publicId: { type: String, required: true }
+}, { 
+  collection: 'horarios',
+  timestamps: true
+})
 
 module.exports = mongoose.model('Horario', HorarioSchema)

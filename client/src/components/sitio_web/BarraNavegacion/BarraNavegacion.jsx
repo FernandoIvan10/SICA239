419015fd9ed429b8e1e4a30e57f85f1e5e23e022
FernandoIvan <fascenciocortes@gmail.com>
2026-01-18 19:46:39 -0600

    Componente BarraNavegacion actualizado

--- client/src/components/sitio_web/BarraNavegacion/BarraNavegacion.jsx
+++ client/src/components/sitio_web/BarraNavegacion/BarraNavegacion.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 // Componente que renderiza la barra de navegación principal del sitio web
 export default function BarraNavegacion(){
-    const [menuAbierto, setMenuAbierto] = useState(false) // Estado del menú lateral (abierto o cerrado)
+    const [menuAbierto, setMenuAbierto] = useState(false) // Controla si el menú lateral está abierto o cerrado
 
     // Métodos para cambiar el estado del menú lateral
     const abrirMenu = () => {
@@ -16,23 +16,23 @@ export default function BarraNavegacion(){
     }
 
     return (
-        <nav class="barra-navegacion">
-            <div className="barra-contenido">
-                <img src={LogoCBTA} alt="Logo" className="img-barra-navegacion"/>
+        <nav className="barra-navegacion">
+            <div className="barra-navegacion__contenido">
+                <img src={LogoCBTA} alt="Logo" className="barra-navegacion__imagen"/>
                 {/* Botón del menú hamburguesa para móviles */}
-                <button className="menu-hamburguesa" onClick={abrirMenu}>
+                <button className="barra-navegacion__icono-menu" onClick={abrirMenu}>
                         ☰
                     </button>
-                <ul className={`ul-barra-navegacion ${menuAbierto ? "activo" : ""}`}>
-                    <li className="cerrar-menu" onClick={cerrarMenu}>✕</li>
-                    <li className="li-barra-navegacion" onClick={cerrarMenu}><Link to="/inicio">Inicio</Link></li>
-                    <li className="li-barra-navegacion" onClick={cerrarMenu}><Link to="/quienes-somos">Quiénes somos</Link></li>
-                    <li className="li-barra-navegacion" onClick={cerrarMenu}><Link to="/oferta-educativa">Oferta educativa</Link></li>
-                    <li className="li-barra-navegacion" onClick={cerrarMenu}><Link to="/contacto">Contacto</Link></li>
-                    <li className="li-barra-navegacion" onClick={cerrarMenu}><Link to="/noticias">Noticias</Link></li>
-                    <li className="li-barra-navegacion" onClick={cerrarMenu}>
+                <ul className={`barra-navegacion__elementos ${menuAbierto ? "activo" : ""}`}>
+                    <li className="barra-navegacion__boton--cerrar" onClick={cerrarMenu}>✕</li>
+                    <li className="barra-navegacion__elemento" onClick={cerrarMenu}><Link to="/inicio">Inicio</Link></li>
+                    <li className="barra-navegacion__elemento" onClick={cerrarMenu}><Link to="/quienes-somos">Quiénes somos</Link></li>
+                    <li className="barra-navegacion__elemento" onClick={cerrarMenu}><Link to="/oferta-educativa">Oferta educativa</Link></li>
+                    <li className="barra-navegacion__elemento" onClick={cerrarMenu}><Link to="/contacto">Contacto</Link></li>
+                    <li className="barra-navegacion__elemento" onClick={cerrarMenu}><Link to="/noticias">Noticias</Link></li>
+                    <li className="barra-navegacion__elemento" onClick={cerrarMenu}>
                         <Link to="/SICA/iniciar-sesion">
-                            <button className="boton-barra-navegacion">Acceso</button>
+                            <button className="barra-navegacion__boton--acceso">Acceso</button>
                         </Link>
                     </li>
                 </ul>

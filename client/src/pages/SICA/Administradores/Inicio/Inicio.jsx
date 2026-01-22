f38fd6c9e3f27df545a99139b50b94d1a5179906
FernandoIvan <fascenciocortes@gmail.com>
2026-01-22 11:35:40 -0600

    Actualización de página Inicio

--- client/src/pages/SICA/Administradores/Inicio/Inicio.jsx
+++ client/src/pages/SICA/Administradores/Inicio/Inicio.jsx
@@ -1,61 +1,56 @@
-import {jwtDecode} from 'jwt-decode'
 import MenuLateral from '../../../../components/sica/MenuLateral/MenuLateral'
 import Bienvenida from '../../../../components/sica/Bienvenida/Bienvenida'
-import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useAuth } from '../../../../auth/useAuth'
 import '../../../../assets/styles/global.css'
 
 // Página de inicio del SICA para administradores
 export default function InicioAdmin(){
-    const navigate = useNavigate() // Para redireccionar a los usuarios
-    const token = localStorage.getItem('token') // Token de inicio de sesión        
-    const [nombreUsuario, setNombreUsuario] = useState('') // Nombre del usuario
-    const [rol, setRol] = useState('') // Tipo de usuario
+    const {usuario} = useAuth() // Usuario autenticado
     const [mensaje, setMensaje] = useState('') // Mensaje de bienvenida
     // Botones para acciones rápidas
     const [boton1, setBoton1] = useState({})
     const [boton2, setBoton2] = useState({})
 
-    useEffect(() => { // Se obtiene el nombre y rol del usuario del token de inicio de sesión
-        try{
-            const tokenDecodificado = jwtDecode(token)
-            setNombreUsuario(tokenDecodificado.nombre)
-            setRol(tokenDecodificado.rol)
-        }catch(error){
-            console.log(error)
-            localStorage.removeItem(token)
-            navigate('/SICA/iniciar-sesion')
-        }
-    },[navigate])
-
     useEffect(() => { // Se asigna un mensaje de bienvenida distinto dependiendo del rol
-            try{
-                if(rol === 'superadmin'){
-                    // Mensaje de bienvenida para el superadmin
-                    setMensaje('Este es el sistema de calificaciones, aquí podrás gestionar los usuarios del sistema, gestionar los grupos y subir las calificaciones de los alumnos')
-                    setBoton1({texto:'Agregar usuario', link:'/SICA/administradores/agregar-usuario'})
-                    setBoton2({texto:'Ver usuarios', link:'/SICA/administradores/ver-usuarios'})
-                }else if(rol==='editor'){
-                    // Mensaje de bienvenida para el editor
-                    setMensaje('Este es el sistema de calificaciones, aquí podrás gestionar los alumnos del sistema, gestionar los grupos y subir las calificaciones de los alumnos')
-                    setBoton1({texto:'Subir calificaciones', link:'/SICA/administradores/calificaciones'})
-                    setBoton2({texto:'Ver grupos', link:'/SICA/administradores/ver-grupos'})
-                } else if(rol==='lector'){
-                    // Mensaje de bienvenida para el lector
-                    setMensaje('Este es el sistema de calificaciones, aquí podrás consultar los alumnos del sistema, las calificaciones, y los grupos')
-                    setBoton1({texto:'Ver calificaciones', link:'/SICA/administradores/calificaciones'})
-                    setBoton2({texto:'Ver usuarios', link:'/SICA/administradores/ver-usuarios'})
-                }
-            }catch(error){
-                console.log(error)
-            }
-    }, [rol])
+        if(usuario.rol === 'superadmin'){
+            setMensaje('Este es el sistema de calificaciones, aquí podrás gestionar los usuarios del sistema, gestionar los grupos y subir las calificaciones de los alumnos')
+            setBoton1({
+                texto:'Agregar usuario', 
+                link:'/SICA/administradores/agregar-usuario'
+            })
+            setBoton2({
+                texto:'Ver usuarios', 
+                link:'/SICA/administradores/ver-usuarios'
+            })
+        }else if(usuario.rol==='editor'){
+            setMensaje('Este es el sistema de calificaciones, aquí podrás gestionar los alumnos del sistema, gestionar los grupos y subir las calificaciones de los alumnos')
+            setBoton1({
+                texto:'Subir calificaciones', 
+                link:'/SICA/administradores/calificaciones'
+            })
+            setBoton2({
+                texto:'Ver grupos', 
+                link:'/SICA/administradores/ver-grupos'
+            })
+        } else if(usuario.rol==='lector'){
+            setMensaje('Este es el sistema de calificaciones, aquí podrás consultar los alumnos del sistema, las calificaciones, y los grupos')
+            setBoton1({
+                texto:'Ver calificaciones', 
+                link:'/SICA/administradores/calificaciones'
+            })
+            setBoton2({
+                texto:'Ver usuarios', 
+                link:'/SICA/administradores/ver-usuarios'
+            })
+        }
+    }, [usuario])
 
     return(
         <div className='contenedor-principal'>
             <MenuLateral/>
             <Bienvenida 
-                nombre={nombreUsuario} 
+                nombre={usuario.nombre}
                 mensaje={mensaje}
                 boton1={boton1.texto}
                 link1={boton1.link}
--- client/src/pages/SICA/Alumnos/Inicio/Inicio.jsx
+++ client/src/pages/SICA/Alumnos/Inicio/Inicio.jsx
@@ -1,19 +1,18 @@
 import MenuLateral from '../../../../components/sica/MenuLateral/MenuLateral'
 import Bienvenida from '../../../../components/sica/Bienvenida/Bienvenida'
-import { jwtDecode } from 'jwt-decode'
+import { useAuth } from '../../../../auth/useAuth'
 import '../../../../assets/styles/global.css'
 
 // Página de inicio del SICA para alumnos
 export default function InicioAlumno(){
-    const token = localStorage.getItem('token') // Token de inicio de sesión
-    const tokenDecodificado = jwtDecode(token) // Datos del token
+    const {usuario} = useAuth() // Usuario autenticado
 
     return(
         <div className="contenedor-principal">
             <MenuLateral/>
             <Bienvenida 
-                nombre={tokenDecodificado.nombre} 
-                descripcion="Este es el sistema de calificaciones, aquí podrás consultar tus calificaciones y tu horario"
+                nombre={usuario.nombre} 
+                mensaje="Este es el sistema de calificaciones, aquí podrás consultar tus calificaciones y tu horario"
                 boton1="Ver calificaciones"
                 link1="/SICA/alumnos/en-curso"
                 boton2="Ver horario"

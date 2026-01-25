f0c13e4fccdeb2db6ab7ccafce08be26aaf3efa5
FernandoIvan <fascenciocortes@gmail.com>
2026-01-25 12:30:29 -0600

    Actualización de componente MenuLateral

--- client/src/components/sica/MenuLateral/MenuLateral.jsx
+++ client/src/components/sica/MenuLateral/MenuLateral.jsx
@@ -1,7 +1,8 @@
 import LogoCBTA from './../../../assets/img/logo_cbta239.png'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { jwtDecode } from 'jwt-decode'
+import { useState } from 'react'
+import { useAuth } from '../../../auth/useAuth'
+import Cargando from '../../../components/sica/Cargando/Cargando'
 
 import '../../../assets/styles/global.css'
 import './MenuLateral.css'
@@ -16,93 +17,11 @@ import { HiLockClosed } from 'react-icons/hi'
 
 // Componente que renderiza el menú lateral del SICA
 export default function MenuLateral(){
-    const navigate = useNavigate() // Para redirigir al usuario
-    const location = useLocation() // Almacena la ruta actual
     const [menuAbierto, setMenuAbierto] = useState(false) // Estado del menú lateral (abierto o cerrado)
-    const [elementosMenu, setElementosMenu] = useState([]) // Elementos del menú lateral (dependen del rol)
-
-    useEffect(() => { // Se asigna el menú dependiendo el rol
-        const token = localStorage.getItem('token')
-        const tokenDecodificado = jwtDecode(token)
-        if(tokenDecodificado.rol === 'alumno'){
-            // Si el usuario es un alumno se asigna el siguiente menú
-            setElementosMenu([
-                {titulo:'Inicio', icono:FaHouseChimney, link:'/SICA/alumnos/inicio'},
-                {titulo:'Calificaciones', icono:MdGrade, 
-                    subelementos:[
-                        {titulo:'En curso', icono:PiMedalFill, link:'/SICA/alumnos/en-curso'},
-                        {titulo:'Historial',icono:FaHistory, link:'/SICA/alumnos/historial'}
-                    ]},
-                {titulo:'Horario', icono:RiCalendarScheduleFill, link:'/SICA/alumnos/horario'},
-                {titulo:'Usuario', icono:FaUserCircle, 
-                    subelementos:[
-                        {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/alumnos/cambiar-contrasena'},
-                        {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
-                    ]}
-            ])
-        }else if(tokenDecodificado.rol === 'superadmin'){
-            // Si el usuario es superadmin se asigna el siguiente menú
-            setElementosMenu([ 
-                {titulo: 'Inicio', icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
-                {titulo: 'Gestionar usuarios', icono:FaUsers, 
-                    subelementos:[
-                        {titulo:'Agregar usuario', icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
-                        {titulo:'Ver usuarios', icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
-                    ]},
-                {titulo: 'Gestionar grupos', icono:FaLayerGroup, 
-                    subelementos:[
-                        {titulo:'Agregar grupo', icono:MdGroupAdd, link:'/SICA/administradores/agregar-grupo'},
-                        {titulo:'Ver grupos', icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
-                        {titulo: 'Subir horarios', icono:RiCalendarScheduleFill, link:'/SICA/administradores/gestionar-horarios'},
-                    ]},
-                {titulo: 'Capturar calificaciones', icono:FaFileUpload, link:'/SICA/administradores/calificaciones'},
-                {titulo: 'Cerrar semestre', icono:HiLockClosed, link:'/SICA/administradores/cerrar-semestre'},
-                {titulo: 'Reubicar alumnos', icono:MdOutlineMoveUp, link:'/SICA/administradores/migrar-alumnos'},
-                {titulo:'Usuario', icono:FaUserCircle, 
-                    subelementos:[
-                        {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/administradores/cambiar-contrasena'},
-                        {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
-                    ]}
-            ])
-        }else if(tokenDecodificado.rol==='editor'){
-            // Si el usuario es editor se asigna el siguiente menú
-            setElementosMenu([ 
-                {titulo: 'Inicio', icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
-                {titulo: 'Gestionar usuarios', icono:FaUsers, 
-                    subelementos:[
-                        {titulo:'Agregar usuario', icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
-                        {titulo:'Ver usuarios', icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
-                    ]},
-                {titulo: 'Gestionar grupos', icono:FaLayerGroup, 
-                    subelementos:[
-                        {titulo:'Agregar grupo', icono:MdGroupAdd, link:'/SICA/administradores/agregar-grupo'},
-                        {titulo:'Ver grupos', icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
-                        {titulo:'Subir horarios', icono:RiCalendarScheduleFill, link:'/SICA/administradores/gestionar-horarios'},
-                    ]},
-                {titulo: 'Capturar calificaciones', icono:FaFileUpload, link:'/SICA/administradores/calificaciones'},
-                {titulo: 'Reubicar alumnos', icono:MdOutlineMoveUp, link:'/SICA/administradores/migrar-alumnos'},
-                {titulo:'Usuario', icono:FaUserCircle, 
-                    subelementos:[
-                        {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/administradores/cambiar-contrasena'},
-                        {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
-                    ]}
-            ])
-        } else if(tokenDecodificado.rol==='lector'){
-            // Si el usuario es lector se asigna el siguiente menú
-            setElementosMenu([ 
-                {titulo: 'Inicio', icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
-                {titulo:'Ver usuarios', icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
-                {titulo:'Ver grupos', icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
-                {titulo: 'Ver horarios', icono:RiCalendarScheduleFill, link:'/SICA/administradores/gestionar-horarios'},
-                {titulo: 'Ver calificaciones', icono:FaFileUpload, link:'/SICA/administradores/calificaciones'},
-                {titulo:'Usuario', icono:FaUserCircle, 
-                    subelementos:[
-                        {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/administradores/cambiar-contrasena'},
-                        {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
-                    ]}
-            ])
-        }
-    }, [navigate])
+    
+    const location = useLocation() // Ruta actual
+    const navigate = useNavigate()
+    const {usuario, logout} = useAuth() // usuario autenticado
 
     // Métodos para cambiar el estado el menú lateral
     const abrirMenu = () => {
@@ -111,35 +30,120 @@ export default function MenuLateral(){
     const cerrarMenu = () => {
         setMenuAbierto(false)
     }
-
+    
     // Función para verificar si la ruta actual coincide con el link del ítem
-    const isActive = (link) => {
+    const estaActivo = (link) => {
         return location.pathname === link ? 'active' : ''
     }
 
     // Método para cerrar la sesión actual
     const cerrarSesion = () => {
-        localStorage.removeItem('token')
+        logout()
         navigate('/SICA/iniciar-sesion')
     }
 
+    const elementosPorRol = { // Elementos del menú dependiendo del rol
+        superadmin: [
+            {titulo: 'Inicio', icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
+            {titulo: 'Gestionar usuarios', icono:FaUsers, 
+                subelementos:[
+                    {titulo:'Agregar usuario', icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
+                    {titulo:'Ver usuarios', icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
+                ]
+            },
+            {titulo: 'Gestionar grupos', icono:FaLayerGroup, 
+                subelementos:[
+                    {titulo:'Agregar grupo', icono:MdGroupAdd, link:'/SICA/administradores/agregar-grupo'},
+                    {titulo:'Ver grupos', icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
+                    {titulo: 'Subir horarios', icono:RiCalendarScheduleFill, link:'/SICA/administradores/gestionar-horarios'},
+                ]
+            },
+            {titulo: 'Capturar calificaciones', icono:FaFileUpload, link:'/SICA/administradores/calificaciones'},
+            {titulo: 'Cerrar semestre', icono:HiLockClosed, link:'/SICA/administradores/cerrar-semestre'},
+            {titulo: 'Reubicar alumnos', icono:MdOutlineMoveUp, link:'/SICA/administradores/migrar-alumnos'},
+            {titulo:'Usuario', icono:FaUserCircle, 
+                subelementos:[
+                    {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/administradores/cambiar-contrasena'},
+                    {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
+                ]
+            }
+        ],
+        editor: [
+            {titulo: 'Inicio', icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
+            {titulo: 'Gestionar usuarios', icono:FaUsers, 
+                subelementos:[
+                    {titulo:'Agregar usuario', icono:TiUserAdd, link:'/SICA/administradores/agregar-usuario'},
+                    {titulo:'Ver usuarios', icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
+                ]
+            },
+            {titulo: 'Gestionar grupos', icono:FaLayerGroup, 
+                subelementos:[
+                    {titulo:'Agregar grupo', icono:MdGroupAdd, link:'/SICA/administradores/agregar-grupo'},
+                    {titulo:'Ver grupos', icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
+                    {titulo:'Subir horarios', icono:RiCalendarScheduleFill, link:'/SICA/administradores/gestionar-horarios'},
+                ]
+            },
+            {titulo: 'Capturar calificaciones', icono:FaFileUpload, link:'/SICA/administradores/calificaciones'},
+            {titulo: 'Reubicar alumnos', icono:MdOutlineMoveUp, link:'/SICA/administradores/migrar-alumnos'},
+            {titulo:'Usuario', icono:FaUserCircle, 
+                subelementos:[
+                    {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/administradores/cambiar-contrasena'},
+                    {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
+                ]
+            }
+        ],
+        lector: [
+            {titulo: 'Inicio', icono:FaHouseChimney, link:'/SICA/administradores/inicio'},
+            {titulo:'Ver usuarios', icono:FaUserEdit, link:'/SICA/administradores/ver-usuarios'},
+            {titulo:'Ver grupos', icono:MdGroups, link:'/SICA/administradores/ver-grupos'},
+            {titulo: 'Ver horarios', icono:RiCalendarScheduleFill, link:'/SICA/administradores/gestionar-horarios'},
+            {titulo: 'Ver calificaciones', icono:FaFileUpload, link:'/SICA/administradores/calificaciones'},
+            {titulo:'Usuario', icono:FaUserCircle, 
+                subelementos:[
+                    {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/administradores/cambiar-contrasena'},
+                    {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
+                ]
+            }
+        ],
+        alumno: [
+            {titulo:'Inicio', icono:FaHouseChimney, link:'/SICA/alumnos/inicio'},
+            {titulo:'Calificaciones', icono:MdGrade, 
+                subelementos:[
+                    {titulo:'En curso', icono:PiMedalFill, link:'/SICA/alumnos/en-curso'},
+                    {titulo:'Historial',icono:FaHistory, link:'/SICA/alumnos/historial'}
+                ]
+            },
+            {titulo:'Horario', icono:RiCalendarScheduleFill, link:'/SICA/alumnos/horario'},
+            {titulo:'Usuario', icono:FaUserCircle, 
+                subelementos:[
+                    {titulo:'Cambiar contraseña', icono:FaKey, link:'/SICA/alumnos/cambiar-contrasena'},
+                    {titulo: 'Cerrar sesión', icono:IoLogOut, onClick: cerrarSesion}
+                ]
+            }
+        ]
+    }
+
+    if(!usuario) return <Cargando/> // Si usuario no existe, no renderizar nada aún
+    
+    const elementosMenu = elementosPorRol[usuario.rol] || [] // Elementos del menú según el rol del usuario
+
     return(
         <>
-            <button className={`menu-hamburguesa ${menuAbierto ? 'oculto' : ""}`} onClick={abrirMenu}>
+            <button className={`menu__icono ${menuAbierto ? 'oculto' : ""}`} onClick={abrirMenu}>
                 ☰
             </button>
-            <div className={`contenedor-menu ${menuAbierto ? 'abierto' : ""}`}>
-                <button className="cerrar-menu" onClick={cerrarMenu}>×</button>
-                <div className="encabezado-menu">
+            <div className={`menu ${menuAbierto ? 'abierto' : ""}`}>
+                <button className="menu__boton--cerrar" onClick={cerrarMenu}>×</button>
+                <div className="menu__encabezado">
                     <img src={LogoCBTA} alt="Logo"/>
                     <strong>SICA239</strong>
                 </div>
-                <div className="elementos-menu">
+                <div className="menu__elementos">
                     <ul>
                         {elementosMenu.map((elemento, index)=>(
                             <li 
                                 key={index} 
-                                className={`elemento ${isActive(elemento.link)}`} 
+                                className={`menu__elemento ${estaActivo(elemento.link)}`} 
                                 onClick={() => {
                                     if (elemento.onClick) {
                                         elemento.onClick()
@@ -150,7 +154,7 @@ export default function MenuLateral(){
                                 }
                             >
                                 <span>
-                                    {elemento.icono && <elemento.icono className="icono"/>}
+                                    {elemento.icono && <elemento.icono className="menu__elemento-icono"/>}
                                     {elemento.titulo}
                                 </span>
                                 {elemento.subelementos && (
@@ -158,7 +162,7 @@ export default function MenuLateral(){
                                         {elemento.subelementos.map((subelemento, subIndex)=>(
                                             <li 
                                                 key={subIndex} 
-                                                className={`subelemento ${isActive(subelemento.link)}`} 
+                                                className={`menu__subelemento ${estaActivo(subelemento.link)}`} 
                                                 onClick={(e) => {
                                                     e.stopPropagation()
                                                     if (subelemento.onClick) {
@@ -169,7 +173,7 @@ export default function MenuLateral(){
                                                 }}
                                             >
                                                 <span>
-                                                    {subelemento.icono && <subelemento.icono className="icono"/>}
+                                                    {subelemento.icono && <subelemento.icono className="menu__elemento-icono"/>}
                                                     {subelemento.titulo}
                                                 </span>
                                             </li>
